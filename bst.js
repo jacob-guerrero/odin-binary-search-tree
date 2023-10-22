0dc1d60c9b96c062acf67a6a521fa474a795015f
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-10-21 20:07:41 -0500

    Add buildTree function and prettyPrint

--- bst.js
+++ bst.js
@@ -8,3 +8,37 @@ const Tree = (arr) => {
   return { root };
 };
 
+const buildTree = (array) => {
+  const createBalancedBST = (arr, start, end) => {
+    if (start > end) {
+      return null;
+    }
+
+    const mid = parseInt((start + end) / 2);
+    const node = createNode(arr[mid]);
+
+    node.left = createBalancedBST(arr, start, mid - 1);
+    node.right = createBalancedBST(arr, mid + 1, end);
+    return node;
+  };
+
+  return createBalancedBST(array, 0, array.length - 1);
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+const BST = Tree(array);
+console.log(BST.root);
+prettyPrint(BST.root);
